4affea0478c0f0e78a58850594eeef90ee9af279
CodeAhmed <mansooronly4u@gmail.com>
2016-07-20 23:36:01 +0530

    Added DAL (Data Access Layer) and two classes, accessing the string and other for returning dataset.

---
+++ MVCWebApp-MyExp/DAL/DALProperty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebApp_MyExp.DAL
+{
+    public class DALProperty : DataAccess
+    {
+        public DataSet functionName()
+        {
+            using (DbCommand ObjCmd = DbDAL.GetStoredProcCommand("YourStoredProcName"))
+            {
+                //DbDAL.AddInParameter(ObjCmd, "@Parameter", DbType.Int32, variable);
+                    return DbDAL.ExecuteDataSet(ObjCmd);
+            }
+        }
+    }
+}
---
+++ MVCWebApp-MyExp/DAL/DataAccess.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using Microsoft.Practices.EnterpriseLibrary.Data.Sql;
+using System.Configuration;
+
+namespace MVCWebApp_MyExp.DAL
+{
+    public class DataAccess
+    {
+        protected Database DbDAL = new SqlDatabase(ConfigurationManager.ConnectionStrings["MVCWebAppExp"].ConnectionString);
+    }
+}
